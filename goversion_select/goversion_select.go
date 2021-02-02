4e798f0c18a58f9a31b11de8298cc8471a630d3d
Will Roden <willabides@github.com>
2021-02-02 12:11:00 -0600

    add exclusive

--- goversion_select/goversion_select.go
+++ goversion_select/goversion_select.go
@@ -25,43 +25,55 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if c == "" {
 		c = "1.x"
 	}
+	exclusive := req.URL.Query().Get("exclusive") != ""
 	constraint, err := goversion.NewConstraints(c)
 	if err != nil {
 		http.Error(w, "invalid constraint", http.StatusBadRequest)
 		return
 	}
 	candidates := req.URL.Query().Get("candidates")
-	var versions []*goversion.Version
-	if candidates != "" {
-		for _, s := range strings.Split(candidates, ",") {
-			var v *goversion.Version
-			v, err = goversion.NewVersion(s)
-			if err != nil || v == nil {
-				continue // silently ignoring bad versions
-			}
-			versions = append(versions, v)
-		}
-	} else {
+	versions := strings2Versions(strings.Split(candidates, ","))
+	result := maxVersionMatch(constraint, versions)
+
+	if !exclusive && result == nil {
 		versions, err = h.getVersions()
 		if err != nil {
 			http.Error(w, "", http.StatusInternalServerError)
 			return
 		}
+		result = maxVersionMatch(constraint, versions)
 	}
+
+	if result == nil {
+		http.Error(w, "no matching version found", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintln(w, result.String())
+}
+
+func strings2Versions(slice []string) []*goversion.Version {
+	result := make([]*goversion.Version, 0, len(slice))
+	for _, s := range slice {
+		v, err := goversion.NewVersion(s)
+		if err != nil {
+			continue
+		}
+		result = append(result, v)
+	}
+	return result
+}
+
+func maxVersionMatch(constraints *goversion.Constraints, versions []*goversion.Version) *goversion.Version {
 	var result *goversion.Version
 	for _, v := range versions {
-		if !constraint.Check(v) {
+		if !constraints.Check(v) {
 			continue
 		}
 		if result == nil || v.GreaterThan(result) {
 			result = v
 		}
 	}
-	if result == nil {
-		http.Error(w, "no matching version found", http.StatusNotFound)
-		return
-	}
-	fmt.Fprintln(w, result.String())
+	return result
 }
 
 func (h *Handler) getVersions() ([]*goversion.Version, error) {
